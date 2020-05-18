7503370aac331da8f09feae7a52700e18a8579a1
Sandro Tsiskadze <stsis15@freeuni.edu.ge>
2020-05-18 21:44:38 +0400

    added tree and changed graph

--- generator/structures/GraphStructure.py
+++ generator/structures/GraphStructure.py
@@ -42,17 +42,12 @@ class GraphStructure:
         if self.loop:
             g = self.add_loop(vertex_count, edge_count, g)
         if self.multi:
-            g = self.to_multi(vertex_count, edge_count, g)
+            g = self.to_multi(edge_count, g)
         if self.vertex_weighted:
-            for node in g.nodes:
-                g.nodes[node]['weight'] = random.randint(
-                    self.vertex_weight_range[0], self.vertex_weight_range[1])
+            g = self.add_weight_to_nodes(g)
         if self.edge_weighted:
-            for edge in g.edges:
-                g.edges[edge]['weight'] = random.randint(
-                    self.edge_weight_range[0], self.edge_weight_range[1])
-        result = list(g.edges)
-        print(len(result))
+            g = self.add_weight_to_edges(g)
+        result = list(g.edges.data('weight'))
 
         return result
 
@@ -69,6 +64,8 @@ class GraphStructure:
             if x == y:
                 continue
             g.add_edge(x, y)
+        if self.directed:
+            g = self.to_directed(g)
         return g
 
     def acyclic_graph(self, vertex_count, edge_count):
@@ -92,26 +89,54 @@ class GraphStructure:
             first_set, secon_set, edge_count, directed=self.directed)
         return g
 
+    def to_directed(self, graph):
+        g = nx.DiGraph()
+        g.add_nodes_from(graph.nodes)
+        edges = list(graph.edges)
+        for edge in edges:
+            coin = random.randint(0, 1)
+            if coin == 0:
+                x = tuple(edge)[0]
+                y = tuple(edge)[1]
+            else:
+                x = tuple(edge)[1]
+                y = tuple(edge)[0]
+            g.add_edge(x, y)
+        return g
+
     def add_loop(self, vertex_count, edge_count, graph):
         edges_left = self.edge_count_range[1]-edge_count
-        added_loops = random.randint(0, edges_left)
-        for _ in range(added_loops):
+        added_edge_count = random.randint(0, edges_left)
+        final_edge_count = edge_count + added_edge_count
+        while graph.number_of_edges() != final_edge_count:
             x = random.randint(0, vertex_count-1)
             graph.add_edge(x, x)
         return graph
 
-    def to_multi(self, vertex_count, edge_count, graph):
+    def to_multi(self, edge_count, graph):
         edges_left = self.edge_count_range[1]-edge_count
-        augmented_edges = random.randint(0, edges_left)
-        print(edges_left, augmented_edges)
-        edges = list(graph.edges)
+        added_edge_count = random.randint(0, edges_left)
+        final_edge_count = edge_count + added_edge_count
         if self.directed:
             g = nx.MultiDiGraph()
         else:
             g = nx.MultiGraph()
         g.add_nodes_from(graph.nodes)
         g.add_edges_from(graph.edges)
-        for _ in range(augmented_edges):
-            x = random.randint(0, vertex_count-1)
+        edges = list(graph.edges)
+        while graph.number_of_edges() != final_edge_count:
+            x = random.randint(0, len(edges)-1)
             g.add_edge(tuple(edges[x])[0], tuple(edges[x])[1])
         return g
+
+    def add_weight_to_nodes(self, graph):
+        for node in graph.nodes:
+            graph.nodes[node]['weight'] = random.randint(
+                self.vertex_weight_range[0], self.vertex_weight_range[1])
+        return graph
+
+    def add_weight_to_edges(self, graph):
+        for edge in graph.edges:
+            graph.edges[edge]['weight'] = random.randint(
+                self.edge_weight_range[0], self.edge_weight_range[1])
+        return graph
--- generator/structures/TreeStructure.py
+++ generator/structures/TreeStructure.py
@@ -4,5 +4,77 @@ import random
 
 class TreeStructure:
     def __init__(self, array=False, connected=False, binary=False, balanced=False, vertex_weighted=False, vertex_weight_range=(0, 10),
-                 edge_weighted=False, edge_weight_range=(0, 10), maximal_children_count=10):
-        pass
+                 edge_weighted=False, edge_weight_range=(0, 10), maximal_children_count=10, vertex_count_range=(0, 10)):
+        self.array = array
+        self.connected = connected
+        self.binary = binary
+        self.balanced = balanced
+        self.vertex_weighted = vertex_weighted
+        self.vertex_weight_range = vertex_weight_range
+        self.edge_weighted = edge_weighted
+        self.edge_weight_range = edge_weight_range
+        self.maximal_children_count = maximal_children_count
+        self.vertex_count_range = vertex_count_range
+
+    def get_tree(self):
+        maximal_children_count = random.randint(0, self.maximal_children_count)
+        vertex_count = random.randint(
+            self.vertex_count_range[0], self.vertex_count_range[1])
+        return self.generate_tree(maximal_children_count, vertex_count)
+
+    def generate_tree(self, maximal_children_count, vertex_count):
+        result = []
+        if self.connected:
+            t = self.connected_tree(maximal_children_count, vertex_count)
+        elif self.binary:
+            t = self.binary_tree(vertex_count)
+        elif self.balanced:
+            t = self.balanced_tree(maximal_children_count, vertex_count)
+        else:
+            t = self.default_tree(maximal_children_count, vertex_count)
+        if self.vertex_weighted:
+            t = self.add_weight_to_nodes(t)
+        if self.edge_weighted:
+            t = self.add_weight_to_edges(t)
+        result = list(t.edges)
+
+        return result
+
+    def default_tree(self, maximal_children_count, vertex_count):
+        return []
+
+    def connected_tree(self, maximal_children_count, vertex_count):
+        degree_sequence = [1]*vertex_count
+        degree_sum = 2*(vertex_count-1)-vertex_count
+        i = 0
+        while degree_sum > 0 and i < vertex_count:
+            if i == vertex_count-1:
+                degree = degree_sum
+            else:
+                degree = random.randint(
+                    0, min(maximal_children_count-1, degree_sum))
+            degree_sequence[i] += degree
+            degree_sum -= degree
+            i += 1
+        t = nx.degree_sequence_tree(degree_sequence)
+        return t
+
+    def binary_tree(self, vertex_count):
+        t = self.connected_tree(2, vertex_count)
+        return t
+
+    def balanced_tree(self, maximal_children_count, vertex_count):
+        t = nx.full_rary_tree(maximal_children_count, vertex_count)
+        return t
+
+    def add_weight_to_nodes(self, graph):
+        for node in graph.nodes:
+            graph.nodes[node]['weight'] = random.randint(
+                self.vertex_weight_range[0], self.vertex_weight_range[1])
+        return graph
+
+    def add_weight_to_edges(self, graph):
+        for edge in graph.edges:
+            graph.edges[edge]['weight'] = random.randint(
+                self.edge_weight_range[0], self.edge_weight_range[1])
+        return graph
